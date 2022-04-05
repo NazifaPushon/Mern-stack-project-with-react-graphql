705aa5078cc033c4985f96c61b87c63b3a649402
NazifaPushon <mashiat.pushon@gmail.com>
2022-04-05 23:19:58 +0600

    apollo setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,25 @@
+import {
+  ApolloClient, ApolloProvider, createHttpLink, InMemoryCache
+} from "@apollo/client";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+const httpLink = createHttpLink({
+  uri:'http://localhost:5000/'
+})
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
